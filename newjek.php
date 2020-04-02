b38919bce8e27aeab75db8bf559a8c8fe625930f
jepriwijayadaulay <63065655+jepriwijayadaulay@users.noreply.github.com>
2020-04-03 00:06:16 +0700

    Newjek01.php
    Voucher baru

--- newjek.php
+++ newjek.php
@@ -52,7 +52,7 @@ function change(){
         echo color("yellow",".");
         sleep(20);
         }
-        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD090320A"}');
+        $code1 = request('/go-promotions/v1/promotions/enrollments', $token, '{"promo_code":"COBAGOFOOD010420A"}');
         $message = fetch_value($code1,'"message":"','"');
         if(strpos($code1, 'Promo kamu sudah bisa dipakai')){
         echo "\n".color("green","+] Message: ".$message);
